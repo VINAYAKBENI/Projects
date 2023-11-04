f8f36399902ff938be494f79ab86f7d72199afe0
VINAYAK BENI <benivinayak2002@gmail.com>
2023-11-05 00:55:44 +0530

    Voice Assistant GPT

---
+++ App Dev Project/Voice Genie/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:voice_genie/home_page.dart';
+import 'package:voice_genie/pallete.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Voice Genie',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Pallete.whiteColor,
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
