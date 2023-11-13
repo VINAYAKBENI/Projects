af19fde438d280963a9eda8419453d3ad2700a93
VINAYAK BENI <benivinayak2002@gmail.com>
2023-11-14 04:19:23 +0530

    Groupie App

---
+++ App Dev Project/Groupie Chat/README.md
@@ -0,0 +1,5 @@
+# groupie
+
+A new Flutter project.
+
+For any type of support contact at email : benivinayak2002@gmail.com 
---
+++ App Dev Project/Groupie Chat/lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:389924872258:web:d41e5172e5efc3c6278ddb";
+  static String apiKey = "AIzaSyDrdKvAT4l3PlT47akhISm3KbI0k_s90zM";
+  static String messagingSenderId = "389924872258";
+  static String projectId = "groupie-55465";
+  final primaryColor = const Color(0xFFee7b64);
+}
